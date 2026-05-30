c398fc5b903237c754bb7311ce22b7ae89f74ac0
EBogu <evan.bogu@gmail.com>
2026-05-30 18:53:09 +0200

    app.py website

---
+++ README.md
@@ -0,0 +1,16 @@
+# API Weather Fetcher
+
+## What it does
+This function prompts the user to input multiple city names. The function then returns to the user in a string format, which city has the highest temperature, highest precipitation and highest humidity at that moment.
+
+## Requirements
+The installation of the requests library is the only prerequisite:
+```
+pip install requests
+```
+
+## How to run
+Call the function in your terminal:
+```
+city_comp()
+```
--- app.py
+++ app.py
@@ -1,27 +1,36 @@
-#API Attempt
-
+from flask import Flask, render_template, request
+from API_weather_fetcher import city_comp
 import requests
+app = Flask(__name__)
 
-def city_comp(city_input):
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    city_input = request.form.get('city')
     city_dict = {}
     x_clean = [z.strip() for z in city_input.split(',')]
     for i in x_clean:
         i_response = requests.get(f'https://wttr.in/{i}?format=j1')
         if i_response.status_code != 200:
-            print(i+' is not a correct city name')
+            pass
         else:
             city_data = i_response.json()
             city_dict[i] = {'temp' : city_data['current_condition'][0]['temp_C'], 
                         'humidity' : city_data['current_condition'][0]['humidity'],
                         'precipMM' : city_data['current_condition'][0]['precipMM']}
-        
     high_temp = max(city_dict, key=lambda city : city_dict[city]['temp'])
     high_precip = max(city_dict, key=lambda city : city_dict[city]['precipMM'])
     high_humid = max(city_dict, key=lambda city : city_dict[city]['humidity'])
-    return f'In {high_temp} the temperature is highest, as it is {city_dict[high_temp]["temp"]} degrees celsius.\nIn {high_precip} the precipitation is the highest, being {city_dict[high_precip]["precipMM"]}mm.\nIn {high_humid} the humidity is highest at {city_dict[high_humid]["humidity"]}%'    
+    return render_template('results.html', 
+    high_temp=high_temp,
+    temp=city_dict[high_temp]['temp'],
+    high_humid=high_humid,
+    humid=city_dict[high_humid]['humidity'],
+    high_precip=high_precip,
+    precip=city_dict[high_precip]['precipMM'])
 
-
-
-    
-    
-
+if __name__ == '__main__':
+    app.run(debug=True)
